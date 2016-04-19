cfdb8ce53458cff371fa265e785af48efe5e0f3b
Warren <kirby027@gmail.com>
2016-04-18 23:41:50 -0400

    This commit will add RiotAPI wrapper and update .gitignore.

--- league.py
+++ league.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
-import RiotAPI
+import rawpi
 import requests
 import json
 
@@ -15,25 +15,27 @@ class league():
 		if summonerName == None:
 			summonerName = str(ctx.message.author.name)
 
-		parsedSumm = RiotAPI.getSummoner("na", summonerName, "689e58e2-23b2-415c-aca7-183ea7fe3535")
+		summonerName = summonerName.replace(" ", "").lower()
+
+		parsedSumm = rawpi.get_summoner_by_name("na", summonerName)
 		if type(parsedSumm) == int: # If something went wrong...
 			await self.bot.say("Failed with error code " + str(parsedSumm) + ". Maybe try turning it off and on again?")
 			return
-		parsedRank = RiotAPI.getRankedData("na", str(parsedSumm[summonerName.lower()]["id"]), "689e58e2-23b2-415c-aca7-183ea7fe3535")
+		parsedRank = rawpi.get_ranked_stats("na", str(parsedSumm[summonerName]["id"]))
 
-		pulledName = parsedSumm[summonerName.lower()]["name"]
-		pulledID = str(parsedSumm[summonerName.lower()]["id"])
-		pulledLevel = str(parsedSumm[summonerName.lower()]["summonerLevel"])
+		pulledName = parsedSumm[summonerName]["name"]
+		pulledID = str(parsedSumm[summonerName]["id"])
+		pulledLevel = str(parsedSumm[summonerName]["summonerLevel"])
 		if type(parsedRank) == int:
 			pulledRankName = "Morello's Subarus"
 			pulledRankTier = "Unranked"
 			pulledDivision = "0"
 			pulledLeaguePoints = "0"
 		else:
-			pulledRankName = parsedRank[str(parsedSumm[summonerName.lower()]["id"])][0]["name"]
-			pulledRankTier = parsedRank[str(parsedSumm[summonerName.lower()]["id"])][0]["tier"].lower().capitalize()
-			pulledDivision = parsedRank[str(parsedSumm[summonerName.lower()]["id"])][0]["entries"][0]["division"]
-			pulledLeaguePoints = str(parsedRank[str(parsedSumm[summonerName.lower()]["id"])][0]["entries"][0]["leaguePoints"])
+			pulledRankName = parsedRank[str(parsedSumm[summonerName]["id"])][0]["name"]
+			pulledRankTier = parsedRank[str(parsedSumm[summonerName]["id"])][0]["tier"].lower().capitalize()
+			pulledDivision = parsedRank[str(parsedSumm[summonerName]["id"])][0]["entries"][0]["division"]
+			pulledLeaguePoints = str(parsedRank[str(parsedSumm[summonerName]["id"])][0]["entries"][0]["leaguePoints"])
 
 		response = "\n__" + pulledName + "__" + "\nLevel " + pulledLevel + " summoner on region NA\n" + \
 					pulledRankTier + " " + pulledDivision + " (" + pulledLeaguePoints + " LP) in " + \
@@ -45,13 +47,13 @@ class league():
 
 	@commands.command(descrption="Lists the free champs of the week")
 	async def freechamps(self):
-		pulledChamps = RiotAPI.getFreeChamps("na", "689e58e2-23b2-415c-aca7-183ea7fe3535")
+		pulledChamps = rawpi.getFreeChamps("na", "689e58e2-23b2-415c-aca7-183ea7fe3535")
 
 		champList = []
 
 		for x in range(10):
 			champID = pulledChamps["champions"][x]["id"]
-			champName = RiotAPI.getChampByID("na", champID, "689e58e2-23b2-415c-aca7-183ea7fe3535")
+			champName = rawpi.getChampByID("na", champID, "689e58e2-23b2-415c-aca7-183ea7fe3535")
 			champList.append(champName)
 
 		response = "The free champions of the week are " + champList[0]
